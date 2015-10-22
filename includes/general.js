b553ad0d6e1edf28649c1868413b92362033feb1
Fares droubi <fares.droubi@gmail.com>
2015-10-22 15:36:23 +0200

    Data format and style

--- includes/general.js
+++ includes/general.js
@@ -58,13 +58,14 @@ function blogHomeDescription(bloghome) {
     return bloghome.getText('bloghome.description') ;
 }
 
-exports.getDate = getDate
+var month_names_short = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
+exports.getDate = getDate
 function getDate(post) {
     var date = post.getDate('post.date')
     if (date) {
-        console.log(date, "post.date")
-        return date
+        var formatedDate = month_names_short[date.getUTCMonth()] + ', ' + date.getDay() + (date.getDay() == 1? 'st ' : 'th ') + date.getUTCFullYear()
+        return formatedDate
     }
 }
 
