df13d5de09bd762beed66ed3182c39f5f3b716e9
Fares Droubi <fdr@zenexity.com>
2015-10-23 11:25:28 +0200

    Covering more cases for the day suffix.

--- includes/general.js
+++ includes/general.js
@@ -3,8 +3,23 @@ var month_names_short = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
 exports.getDate = getDate
 function getDate(post) {
     var date = post.getDate('post.date')
+    var suffix = ''
+    switch (date.getUTCDate().toString().substr(date.getUTCDate().toString().length - 1)) {
+        case '1':
+            suffix = 'st';
+            break;
+        case '2':
+            suffix = 'nd';
+            break;
+        case '3':
+            suffix = 'rd';
+            break;
+        default :
+            suffix = 'th'
+    }
+
     if (date) {
-        var formatedDate = month_names_short[date.getUTCMonth()] + ', ' + date.getDay() + (date.getDay() == 1? 'st ' : 'th ') + date.getUTCFullYear()
+        var formatedDate = month_names_short[date.getUTCMonth()] + ', ' + date.getUTCDate() + suffix + ' '+ date.getUTCFullYear()
         return formatedDate
     }
 }
