953dc0033d5e986d30b2ac5abcd1c67b20582052
Fares Droubi <fdr@zenexity.com>
2015-11-25 18:59:06 +0100

    More concise code.

--- blog.js
+++ blog.js
@@ -1,6 +1,30 @@
 var Prismic = require('express-prismic').Prismic,
     configuration = require('./prismic-configuration').Configuration;
 
+exports.bloghome = function(req, res) {
+
+  var p = Prismic.withContext(req, res);
+  p.queryFirst('[[:d = at(document.type, "bloghome")]]', function (err, bloghome) {
+    if (err) { return configuration.onPrismicError(err, req, res); }
+    if (bloghome) {
+      var page = currentPage(req);
+      var options = {
+        'page' : page,
+        'orderings' :'[my.post.date desc]'
+      };
+      p.query('[[:d = at(document.type, "post")]]', options, function (err, response) {
+        if (err) { return configuration.onPrismicError(err, req, response); }
+        res.render('bloghome', {
+          'bloghome' : bloghome,
+          'posts' : response.results
+        });
+      });
+    } else {
+      res.status(404)
+        .send('Not found');
+    }
+  });
+};
 
 exports.post = function(req, res) {
 
@@ -15,40 +39,6 @@ exports.post = function(req, res) {
     } else {
       res.status(404)
         .send('Not found');
-    }
-  });
-};
-
-exports.bloghome = function(req, res) {
-  Prismic.withContext(req, res, function then(error, ctx) {
-    if (error) { configuration.onPrismicError(error, req, res); return; }
-    var bloghomeId = ctx.api.bookmarks.bloghome;
-
-    if(bloghomeId === undefined) {
-      req.status(404)
-        .send('Not found');
-    } else {
-      var page = currentPage(req);
-      var options = {
-        'page' : page,
-        'orderings' :'[my.post.date desc]'
-      };
-      var p = Prismic.withContext(req, res);
-      p.getByID(bloghomeId, function (err, bloghome) {
-        if (err) { return configuration.onPrismicError(err, req, res); }
-        if (bloghome) {
-          p.query('[[:d = at(document.type, "post")]]', options, function (err, response) {
-            if (err) { return configuration.onPrismicError(err, req, response); }
-            res.render('bloghome', {
-              'bloghome' : bloghome,
-              'posts' : response.results
-            });
-          });
-        } else {
-          res.status(404)
-            .send('Not found');
-        }
-      });
     }
   });
 };
