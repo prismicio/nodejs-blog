eed9f14f91b1a582821e01f1eae38124690c3add
Erwan Loisant <eloisant@gmail.com>
2016-02-17 13:53:29 +0100

    Revert "reset config"
    This reverts commit 238a8d2926f2762b49f09c6341b09b4bf13a6c15.

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,6 +1,6 @@
 exports.Configuration = {
 
-  apiEndpoint: 'https://your-repo-name.prismic.io/api',
+  apiEndpoint: 'https://nodejs-blog.prismic.io/api',
 
   // -- Access token if the Master is not open
   // accessToken: 'xxxxxx',
