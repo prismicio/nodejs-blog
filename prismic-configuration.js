16de0d3ec49cc0960c7cafb2767adbbff34b8de3
Priyanka Maheshwari <priyankamaheshwari.2021@gmail.com>
2021-12-08 23:38:11 +0100

    Change method of initiate Client

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -4,28 +4,25 @@
 import fetch from 'node-fetch';
 import * as prismic from '@prismicio/client';
 
-const repoName = `newnodeblog`; // Fill in your repository name.
-const accessToken = ``; // If your repo is private, add an access token.
+export const repoName = `newnodeblog`; // Fill in your repository name.
 const endpoint = prismic.getEndpoint(repoName); // Format your endpoint.
 
-// The `routes` property is your Route Resolver. It defines how you will 
-// structure URLs in your project. Update the types to match the Custom 
-// Types in your project, and edit the paths to match the routing in your 
-// project.
-const routes = [
-  {
-    type: 'blog_home',
-    path: '/'
-  },
-  {
-    type: 'post',
-    path: '/blog/:uid'
-  },
-]
+export const client = prismic.createClient(endpoint, {
+  // If your repo is private, add an access token.
+  accessToken: ``,
+  fetch,
 
-const createClient = (req) => {
-  const client = prismic.createClient(endpoint, { fetch, accessToken,routes, req})
-  return client
-}
-
-export default createClient;
+  // This defines how you will structure URL paths in your project.
+  // Update the types to match the Custom Types in your project, and edit
+  // the paths to match the routing in your project.
+  routes: [
+    {
+      type: 'blog_home',
+      path: '/'
+    },
+    {
+      type: 'post',
+      path: '/blog/:uid'
+    },
+  ],
+});
